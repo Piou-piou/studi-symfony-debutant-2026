ff92007b1bf9c3c245e79332a10a45780b37a121
Piou-piou <pilloud.anthony@gmail.com>
2026-01-26 18:59:37 +0100

    feat: push first controller route with twig

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class IndexController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('index/index.html.twig', [
+            'controller_name' => 'Mon controller index',
+            'firstnames' => [
+                'anthony',
+                'bruno',
+                'pierre',
+            ]
+        ]);
+    }
+
+    // exemple de route multiple
+    #[Route('/create', name: 'create')]
+    #[Route('/edit/{firstname}/{lastname}', name: 'edit')]
+    public function edit(?string $firstname, ?string $lastname = null): Response
+    {
+        return new Response('Hello '.$firstname.' '.$lastname.' !');
+    }
+}
