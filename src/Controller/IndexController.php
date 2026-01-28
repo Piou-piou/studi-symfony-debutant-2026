f815bfac81eae4cb2c29b0ab2c93743b4911f4c9
Piou-piou <pilloud.anthony@gmail.com>
2026-01-28 19:01:49 +0100

    feat: add first entity

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,23 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class IndexController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
+        $articles = $em->getRepository(Article::class)->findAll();
+
         return $this->render('index/index.html.twig', [
             'controller_name' => 'Mon controller index',
-            'firstnames' => [
-                'anthony',
-                'bruno',
-                'pierre',
-            ]
+            'articles' => $articles
         ]);
     }
 
